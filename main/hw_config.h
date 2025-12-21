a300bf16128a6bae8955c288118a5ac91edb71bf
george benett <georgebenett@gmail.com>
2025-12-21 18:23:55 +0100

    add telemetry to ui config tool

--- main/hw_config.h
+++ main/hw_config.h
@@ -12,8 +12,8 @@
 #define AUX_OUTPUT_GPIO           GPIO_NUM_6
 #define AUX_OUTPUT_LED_PIN        GPIO_NUM_10
 
-#define BMS_UART_TX_PIN           GPIO_NUM_17
-#define BMS_UART_RX_PIN           GPIO_NUM_16
+#define BMS_UART_TX_PIN           GPIO_NUM_16
+#define BMS_UART_RX_PIN           GPIO_NUM_17
 
 // UART configurations
 #define UART1_VESC_BAUD_RATE      115200
