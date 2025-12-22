ec2b2b99098125f4c635d48cf69bba7017ea8b8e
george benett <georgebenett@gmail.com>
2025-12-22 22:16:25 +0100

    remove excessive comments

--- main/version.h
+++ main/version.h
@@ -1,10 +1,7 @@
 #pragma once
 
-// Firmware version
 #define FW_VERSION "1.0.0"
 
-// Build date and time (these would typically be set by build system)
-// For now, using placeholders - can be replaced with __DATE__ and __TIME__ if needed
 #define BUILD_DATE __DATE__
 #define BUILD_TIME __TIME__
 
