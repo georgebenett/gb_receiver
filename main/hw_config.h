76a03c76e403df6553a0dc3b85f07efa6d819783
george benett <georgebenett@gmail.com>
2026-02-28 14:49:27 +0100

    fix bms uart port inversion

--- main/hw_config.h
+++ main/hw_config.h
@@ -10,8 +10,8 @@
 #define AUX_OUTPUT_GPIO           GPIO_NUM_1
 #define AUX_OUTPUT_LED_PIN        GPIO_NUM_0
 
-#define BMS_UART_TX_PIN           GPIO_NUM_6
-#define BMS_UART_RX_PIN           GPIO_NUM_7
+#define BMS_UART_TX_PIN           GPIO_NUM_7
+#define BMS_UART_RX_PIN           GPIO_NUM_6
 
 // UART configurations
 #define BMS_UART_PORT             UART_NUM_1
